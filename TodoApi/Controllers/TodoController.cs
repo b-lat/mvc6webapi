ab3c7e8852f3d61f376b5483d02d7648ffa69496
b-lat <ben.a.latimer@gmail.com>
2016-03-24 15:21:15 +0000

    Initial commit

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    using TodoApi.Models;
+
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        // FromServices attribute tells MVC to inject the ITodoRepository that we registered in the Startup class.
+        [FromServices]
+        public ITodoRepository TodoItems { get; set; }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return TodoItems.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = TodoItems.Find(id);
+            if (item == null)
+            {
+                return this.HttpNotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        // FromBody tells MVC to get the valuie of the to-do item from the body of the HTTP request
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return this.HttpBadRequest();
+            }
+            TodoItems.Add(item);
+            return CreatedAtRoute("GetTodo", new { controller = "Todo", id = item.Key }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return this.HttpBadRequest();
+            }
+
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            TodoItems.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            TodoItems.Remove(id);
+        }
+
+    }
+}
---
+++ TodoApi/Models/ITodoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public interface ITodoRepository
+    {
+        void Add(TodoItem item);
+
+        IEnumerable<TodoItem> GetAll();
+
+        TodoItem Find(string key);
+
+        TodoItem Remove(string key);
+
+        void Update(TodoItem item);
+    }
+}
